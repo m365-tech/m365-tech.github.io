35dbeea402f710cfd04f8144145a938cdee7df79
MarkD <mark.doria@outlook.com>
2024-07-01 19:49:28 -0400

    change default settings

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,10 +8,11 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://m365-tech.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
+  trailingSlash: false,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
