19054f314458d7f9d823db2fd42682ba290902bf
MarkD <mark.doria@outlook.com>
2024-07-01 20:33:22 -0400

    Update settings

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'm365-tech', // Usually your GitHub org/user name.
   projectName: 'm365-tech.github.io', // Usually your repo name.
-
+  deploymentBranch: 'gh-pages',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
